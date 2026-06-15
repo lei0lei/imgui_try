1a77bb1d31af5a570bdc99366b27cee1068d69d9
meng fanlei <lei.lei.fan.meng@gmail.com>
2026-06-15 14:10:13 +0800

    init

--- cmake/local_paths.cmake
+++
@@ -1,6 +0,0 @@
-# Machine-local paths for this computer (not committed to git).
-
-set(SDL3_SOURCE_DIR "D:/github/SDL" CACHE PATH "Path to SDL3 source tree (contains CMakeLists.txt)" FORCE)
-set(IMGUITRY_VOLK_SOURCE_DIR "D:/github/volk" CACHE PATH "Path to volk source tree (contains CMakeLists.txt)" FORCE)
-set(LUA_SOURCE_DIR "D:/p90x/lua-5.5.0.tar/lua-5.5.0" CACHE PATH "Path to Lua source tree (contains CMakeLists.txt)" FORCE)
-set(DOXYGEN_EXECUTABLE "C:/Program Files/doxygen/bin/doxygen.exe" CACHE FILEPATH "Path to Doxygen executable (optional)" FORCE)
